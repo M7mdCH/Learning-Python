4e44d6d594a37bb12031e8daf0dfd73c69a6d55e
Mohammed Chekhab <technorindroid@gmail.com>
2020-04-11 18:05:00 +0200

    Add files via upload

---
+++ houcem.py
@@ -0,0 +1,24 @@
+author = 'M7md'
+print('Hello Houcem')
+import time
+time.sleep(5)
+print('Are you okay there in such dangerous conditions ?')
+okay=input('y/n   ')
+if (okay) == 'y':
+    print('Thank Allah, Here are some tips for avoiding the virus')
+    time.sleep(1)
+    print('STAY at home as much as you can')
+    time.sleep(1)
+    print('KEEP a safe distance')
+    time.sleep(1)
+    print('WASH your hands')
+    input('Press Enter to exit')
+if (okay) == 'n':
+    print('rest and sleep')
+    time.sleep(1)
+    print('keep warm')
+    time.sleep(1)
+    print('drink plenty of liquids')
+    time.sleep(1)
+    print('use a room humidifier or take a hot shower to help ease a sore throat and cough')
+    input('Press Enter to exit')
